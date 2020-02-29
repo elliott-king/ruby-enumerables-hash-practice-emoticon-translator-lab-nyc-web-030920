dea5e91a31b5a6763a235c4e03f63f0af46963f8
Elliott King <8884571+elliott-king@users.noreply.github.com>
2020-02-29 22:29:51 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,7 +2,12 @@ require "yaml"
 
 
 def load_library fp
-  puts YAML.load_file(fp)
+  translator = {'get_meaning' => {}, 'get_emoticon' => {}}
+  lib =  YAML.load_file(fp)
+  lib.each do |k, (english, japanese)|
+    translator['get_meaning'][japanese] = k
+    translator['get_emoticon'][english] = japanese
+  end
 end
 
 def get_japanese_emoticon
